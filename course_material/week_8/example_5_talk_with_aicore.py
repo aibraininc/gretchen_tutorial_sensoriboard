1243a259bacd26f3dbae02f3f9efc3401c756f6e
passionbull <wnsl5684@gmail.com>
2020-03-30 12:57:00 +0900

    added example_5 at week_8

--- course_material/week_8/example_5_talk_with_aicore.py
+++ course_material/week_8/example_5_talk_with_aicore.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python
+import sys
+import speech_recognition as sr
+sys.path.append('..')
+from lib.ros_environment import ROSEnvironment
+from lib.aicore_client import AICoRE
+
+def main():
+    #We need to initalize ROS environment for AICoRE to connect
+    ROSEnvironment()
+    client = AICoRE()
+    client.setUserName('Jacob')
+
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    text = r.recognize_google(audio)
+    client.send(text)
+    client.answer()
+
+if __name__ == '__main__':
+    main()
+
+
