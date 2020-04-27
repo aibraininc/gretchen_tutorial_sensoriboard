53b5517bf51e9e157b7f6ca913ecc8406f492bb7
passionbull <wnsl5684@gmail.com>
2020-04-27 19:11:25 +0900

    updated image showing and ball detect test

---
+++ course_material/week_5/example_2_detect_ball_with_hough.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import cv2
+import sys
+from example_2_ball_detector import BallDetector
+sys.path.append('..')
+from lib.camera_v2 import Camera
+from lib.ros_environment import ROSEnvironment
+import numpy as np
+
+def main():
+    #We need to initalize ROS environment for Robot and camera to connect/communicate
+    ROSEnvironment()
+    #Initalize camera
+    camera = Camera()
+    #Start camera
+    camera.start()
+    #Initalize ball detector
+    ball_detector = BallDetector()
+
+    #loop
+    while True:
+        #get image
+        img = camera.getImage()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.erode(gray, None, iterations=2)
+        gray = cv2.dilate(gray, None, iterations=2)
+
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.4, 100)
+
+
+        if circles is not None:
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                # draw the circle in the output image, then draw a rectangle
+                # corresponding to the center of the circle
+                cv2.circle(img, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            # show the output image
+        cv2.imshow("Frame", img[...,::-1])
+        key = cv2.waitKey(1)
+        if key > 0:
+            break
+
+if __name__ == '__main__':
+    main()
--- course_material/week_9/example_2_tracking_object_you_said_v2.py
+++ course_material/week_9/example_2_tracking_object_you_said_v2.py
@@ -178,7 +178,7 @@ def main():
             conf_value = confidence_values[i]
             draw_boundingbox(input_image, classid, conf_value, round(x), round(y), round(x+w), round(y+h))
 
-        cv2.imshow("Object Detection Window", input_image)
+        cv2.imshow("Object Detection Window", input_image[...,::-1])
         key = cv2.waitKey(1)
         cv2.imwrite("detected_object.jpg", input_image)
     cv2.destroyAllWindows()
