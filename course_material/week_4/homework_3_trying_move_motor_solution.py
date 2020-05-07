1cbeed4b1d528778d464e94fdc54caff363ac6b6
Jacob <wnsl5684@gmail.com>
2020-05-07 13:41:58 +0900

    updated week4 text

--- course_material/week_4/homework_3_trying_move_motor_solution.py
+++ course_material/week_4/homework_3_trying_move_motor_solution.py
@@ -24,14 +24,14 @@ def main():
         time.sleep(0.1)
     robot.center()
     for i in range(10):
-        #TODO: change the values in right
+        #TODO: change the values in up
         robot.up(0.2)
         time.sleep(0.1)
     for i in range(10):
-        #TODO: change the values in right
+        #TODO: change the values in down
         robot.down(0.2)
         time.sleep(0.1)
 
-    #TODO write code to make the robot go up and down
+    #TODO: write code to make the robot go up and down
 if __name__ == '__main__':
     main()
