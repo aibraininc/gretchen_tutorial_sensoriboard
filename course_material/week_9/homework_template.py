3ad9ec306030e91cb153ee8f422c5580c9741df0
Jacob <wnsl5684@gmail.com>
2020-05-26 17:26:45 +0900

    added threading homework_template

--- course_material/week_9/homework_template.py
+++ course_material/week_9/homework_template.py
@@ -26,12 +26,33 @@ from lib.aicore_client import AICoRE
 from lib.camera_v2 import Camera
 from lib.robot import Robot
 
+
+def listen():
+    while True:
+        r = sr.Recognizer()
+        mic = sr.Microphone(device_index=11)
+        print("I am ready to listen.")
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+        except:
+            pass
+
+
 def main():
     ROSEnvironment()
     camera = Camera()
     camera.start()
     robot = Robot()
     robot.start()
+
+    # start threading for speech recognition
+    t1 = threading.Thread(target=listen)
+    t1.daemon = True
+    t1.start()
 
     #loops
     while(True):
